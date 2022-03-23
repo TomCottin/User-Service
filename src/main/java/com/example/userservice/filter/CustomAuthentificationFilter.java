f41d89d23ee5cef3a4f6779579ce4b9dbef61640
Tom Cottin <ctom.99@outlook.fr>
2022-03-23 10:48:03 +0100

    Access & refresh tokens working (set in header)

--- src/main/java/com/example/userservice/filter/CustomAuthentificationFilter.java
+++ src/main/java/com/example/userservice/filter/CustomAuthentificationFilter.java
@@ -1,16 +1,23 @@
 package com.example.userservice.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -34,8 +41,24 @@ public class CustomAuthentificationFilter extends UsernamePasswordAuthentication
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+        String accessToken = JWT.create()
+            .withSubject(user.getUsername()) // Can be anything belonging to the user (ID, username, email ...), needs to be unique !
+            .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+            .withIssuer(request.getRequestURL().toString()) //
+            .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+            .sign(algorithm);
+
+        String refreshToken = JWT.create()
+        .withSubject(user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) // Can be longer than the access token (week, month, year), depends on the application and the security level needed
+        .withIssuer(request.getRequestURL().toString())
+        .sign(algorithm);
+
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refreshToken);
     }
 
 }
