1ef1c0181a4144ca44a9611c6ba82ba867ccfaf8
Tom Cottin <ctom.99@outlook.fr>
2022-03-23 11:24:20 +0100

    Requests authorization update

--- src/main/java/com/example/userservice/security/SecurityConfig.java
+++ src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -7,37 +7,49 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-import com.example.userservice.filter.CustomAuthentificationFilter;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+import com.example.userservice.filter.CustomAuthentificationFilter;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-    
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final UserDetailsService userDetailsService;
-	private final BCryptPasswordEncoder bCryptpasswordEncoder;
+    private final BCryptPasswordEncoder bCryptpasswordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptpasswordEncoder);
     }
 
+    // In this function the order of the lines matter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthentificationFilter customAuthentificationFilter = new CustomAuthentificationFilter(authenticationManagerBean());
+        customAuthentificationFilter.setFilterProcessesUrl("/api/v1/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthentificationFilter(authenticationManagerBean()));
+        // Line below needs to be first
+        http.authorizeRequests().antMatchers("/api/v1/login/**").permitAll(); // Anyone can access this ressource
+        // Then set the who can access what
+        http.authorizeRequests().antMatchers(GET, "/api/v1/users/**").hasAnyAuthority("ROLE_USER"); // Only app users with ROLE_USER can access this ressource
+        http.authorizeRequests().antMatchers(POST, "/api/v1/user/save/**").hasAnyAuthority("ROLE_ADMIN"); // Only app users with ROLE_ADMIN can access this ressource
+        // Finally everyone needs to be authenticated to make a request
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthentificationFilter);
     }
 
-    @Bean @Override
+    @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
